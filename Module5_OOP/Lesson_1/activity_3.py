fc5fe3aabfb7d872b2b386a2b20f84fa60a20a47
ishaan-coder-feb <goyalishaan007@gmail.com>
2024-11-14 10:28:00 +0400

    add activity_3.py file

--- Module5_OOP/Lesson_1/activity_3.py
+++ Module5_OOP/Lesson_1/activity_3.py
@@ -13,13 +13,18 @@ print("{}is {}years old".format(woo.name,woo.age))
 =======
 class Parrot:
     species="bird"
-    def__init__(self,name,age):
-        self.name=name
-        self.age=age
+    def__init__(self,name,age)
+      self.name=name
+      self.age=age
 blu=Parrot("blu",10)
 woo=Parrot("woo",12)
 print("Blu is a {}".format(blu.species))
 print("woo is also a {}".format(woo.species))
 print("{}is {}years old".format(blu.name,blu.age))
 print("{}is {}years old".format(woo.name,woo.age))
+<<<<<<< HEAD
 >>>>>>> 15e1069 (add activity_3.py)
+=======
+
+
+>>>>>>> 4430d46 (add AFC.py file)
--- Module5_OOP/Lesson_3/activity_1.py
+++ Module5_OOP/Lesson_3/activity_1.py
@@ -9,6 +9,7 @@ class son(dad):
     def __init__(self,name,age,eyes,aggressive):
         self.name=name
         self.age=age
-        dad.__init__(self,eyes,aggressive)
+        # dad.__init__(self,eyes,aggressive)
+        super(). __init__(var1,var2)
 obj=son("Ishaan",11,"Black",False)  
 obj.display()      
