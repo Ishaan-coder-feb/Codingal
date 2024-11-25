2782badc1826c6444f6e56e9d97741aade50762b
ishaan-coder-feb <goyalishaan007@gmail.com>
2024-11-25 20:06:19 +0400

    add activity_3.py

--- Module5_OOP/Lesson_1/activity_3.py
+++ Module5_OOP/Lesson_1/activity_3.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 class Parrot:
     species="bird"
     def__init__(self,name,age)
@@ -11,28 +9,3 @@ print("Blu is a {}".format(blu.species))
 print("woo is also a {}".format(woo.species))
 print("{}is {}years old".format(blu.name,blu.age))
 print("{}is {}years old".format(woo.name,woo.age))
-=======
-=======
->>>>>>> 4430d4631d75afe13fe6d031fe48f3938c2997a1
-class Parrot:
-    species="bird"
-    def__init__(self,name,age)
-      self.name=name
-      self.age=age
-blu=Parrot("blu",10)
-woo=Parrot("woo",12)
-print("Blu is a {}".format(blu.species))
-print("woo is also a {}".format(woo.species))
-print("{}is {}years old".format(blu.name,blu.age))
-print("{}is {}years old".format(woo.name,woo.age))
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 15e1069 (add activity_3.py)
-=======
-
-
->>>>>>> 4430d46 (add AFC.py file)
-=======
-
-
->>>>>>> 4430d4631d75afe13fe6d031fe48f3938c2997a1
